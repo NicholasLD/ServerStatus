1d153c2d2d389f13345cf7d78e9cba7877e8f524
NicholasLD <878639947@qq.com>
2022-11-29 01:53:03 +0800

    init

--- extend.php
+++ extend.php
@@ -14,12 +14,11 @@ namespace NicholasLD\ServerStatus;
 use Flarum\Extend;
 
 return [
-
     (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/js/dist/admin.js'),
-        ->css(__DIR__.'/less/admin.less')
+        ->js(__DIR__.'/js/dist/admin.js')
+        ->css(__DIR__.'/less/admin.less'),
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
-        ->css(__DIR__.'/less/forum.less')
+        ->css(__DIR__.'/less/forum.less'),
 
 ];
