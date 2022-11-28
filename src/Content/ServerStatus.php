0d7e6de24c394c45515b920474918852c7459b5b
NicholasLD <878639947@qq.com>
2022-11-29 02:20:41 +0800

    init

--- src/Content/ServerStatus.php
+++ src/Content/ServerStatus.php
@@ -5,10 +5,15 @@ namespace NicholasLD\ServerStatus\Content;
 use Flarum\Api\Client;
 use Flarum\Frontend\Document;
 use Flarum\Http\UrlGenerator;
+use Flarum\Settings\SettingsRepositoryInterface;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Support\Arr;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ServerStatus
 {
-    public function __invoke(Document $document, Request $request)
+    public function __invoke(Document $document, Request $request): Document
     {
         //加载页面Helloworld
         $document->content = '<div id="server-status">Hello</div>';
